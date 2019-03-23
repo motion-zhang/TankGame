821a66b3f6513b1613e58e6f0a4453c2e4e1e219
shanbc <shan.bo@husky.neu.edu>
2019-03-23 15:00:23 -0400

    Interface of Tank model.

---
+++ TankGame/src/TankModel/ITankModel.java
@@ -0,0 +1,19 @@
+package TankModel;
+
+import tank.Tank;
+
+public interface ITankModel {
+
+  //Get the player's tank at that time.
+  public Tank getPlayerTank();
+
+  //Move the tank by the certain direction.
+  public void move(int direction);
+
+  //Determine if the game is over.
+  // 0 for keep running, 1 for win, 2 for lose..
+  public int isWin();
+
+  //Let the player;s tank fire.
+  public void playerFire();
+}
